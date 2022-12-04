49cb2b9bf6d3731ce44e4e9d504c0b51e789edbf
Jonas Pereira <jonaspplc@hotmail.com>
2022-12-04 16:24:27 -0300

    working balance by month chart

--- src/views/components/pages/Dashboard/Charts.vue
+++ src/views/components/pages/Dashboard/Charts.vue
@@ -4,15 +4,17 @@
       <v-col cols="4"  max-width="calc(100% - 32px)">
         <by-category-chart-bar></by-category-chart-bar>
       </v-col>
+    </v-row>
+    <v-row>
       <v-col cols="4"  max-width="calc(100% - 32px)">
         <expenses-by-month></expenses-by-month>
       </v-col>
       <v-col cols="4"  max-width="calc(100% - 32px)" max-heigth="150px">
         <incomes-by-month></incomes-by-month>
       </v-col>
-    </v-row>
-    <v-row>
-
+      <v-col cols="4"  max-width="calc(100% - 32px)" max-heigth="150px">
+        <balance-by-month></balance-by-month>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -21,12 +23,14 @@
 import ByCategoryChartBar from "./Charts/ExpensesByCategory.vue";
 import ExpensesByMonth from "./Charts/ExpensesByMonth.vue";
 import IncomesByMonth from "./Charts/IncomesByMonth.vue";
+import BalanceByMonth from "./Charts/BalanceByMonth.vue";
 
 export default {
   components: {
     ByCategoryChartBar: ByCategoryChartBar,
     ExpensesByMonth: ExpensesByMonth,
     IncomesByMonth:IncomesByMonth,
+    BalanceByMonth:BalanceByMonth,
   },
 };
 </script>
---
+++ src/views/components/pages/Dashboard/Charts/BalanceByMonth.vue
@@ -0,0 +1,209 @@
+<template>
+  <div>
+    <v-card class="mx-auto">
+      <v-sheet
+        class="v-sheet--offset mx-auto pa-8"
+        style="background: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)"
+        elevation="12"
+        max-width="calc(100% - 32px)"
+      >
+        <LineChartGenerator
+          :chart-options="chartOptions"
+          :chart-data="chartData"
+          :chart-id="chartId"
+          :dataset-id-key="datasetIdKey"
+          :plugins="plugins"
+          :css-classes="cssClasses"
+          :styles="styles"
+          :width="width"
+          :height="height"
+        />
+      </v-sheet>
+
+      <div>
+        <div class="ml-5">
+          <h3 class="font-weight-thin" color="grey">Saldo por mês</h3>
+          <br />
+          <br />
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement,
+} from "chart.js";
+import { Line as LineChartGenerator } from "vue-chartjs/legacy";
+import { mapActions, mapState, mapGetters, mapMutations } from "vuex";
+import moment from "moment";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+);
+
+export default {
+  name: "LineChart",
+  components: {
+    LineChartGenerator,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: "line-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    width: {
+      type: Number,
+      default: 300,
+    },
+    height: {
+      type: Number,
+      default: 215,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+      month: 1,
+      year: 2021,
+
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        borderColor: ["rgba(0, 255, 0, 0.4)"],
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        pointBackgroundColor: ["rgba(0, 255, 0, 0.4)"],
+        pointHitRadius: 40,
+        scales: {
+          x: {
+            // display: false,
+            grid: {
+              borderColor: "white",
+              display: false,
+            },
+            ticks: {
+              color: "white",
+            },
+          },
+          y: {
+            // display: false,
+            grid: {
+              borderColor: "transparent",
+              display: false,
+            },
+            ticks: {
+              display: true,
+              color: "white",
+            },
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    ...mapGetters({
+      months: "months",
+      incomesByMonthChart: "incomesByMonthChart",
+    }),
+    me() {
+      return this.$store.getters.me;
+    },
+    labels() {
+      return this.$store.getters.incomesByMonthChart.labels;
+    },
+    values() {
+      return this.$store.getters.incomesByMonthChart.values;
+    },
+    chartData() {
+      return {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: this.values,
+          },
+        ],
+      };
+    },
+  },
+  methods: {
+    ...mapActions(["getIncomesByMonthChartBar"]),
+    ...mapMutations(["SET_DATE_FILTER", "SET_PRELOADER"]),
+
+    setDateToday() {
+      const date = new Date();
+      this.year = date.getFullYear();
+      this.month = date.getMonth() + 1;
+    },
+    formatDateYearMonthFromApi() {
+      var date = moment(this.year + "-" + this.month).format("YYYY-MM");
+      return date;
+    },
+
+    getChartByMonth() {
+      this.isLoading = true;
+      const date = this.formatDateYearMonthFromApi();
+
+      setTimeout(() => {
+        const params = {
+          userId: this.me.id,
+          due_date: date,
+        };
+
+        this.getIncomesByMonthChartBar(params)
+          .then((res) => {})
+          .finally(() => {
+            this.isLoading = false;
+          });
+      }, 5000);
+    },
+  },
+  created() {
+    this.setDateToday();
+    this.getChartByMonth();
+  },
+  watch: {},
+};
+</script>
+
+<style>
+.v-sheet--offset {
+  top: -24px;
+  position: relative;
+}
+</style>
